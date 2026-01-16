6ce93710a35c81f26b305240eab46de76d87b929
Ace <60907980+AceT979@users.noreply.github.com>
2026-01-16 22:52:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('.slide-right, .fade-up');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  },
+  {
+    threshold: 0.25
+  }
+);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
